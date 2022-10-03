d1db8d4bb3d7f209eefa2083aeaf8479524088b6
tinapayshakera <tinapayshakera@gmail.com>
2022-10-03 09:58:00 +0800

    added folder

---
+++ Second_Activity/SecondAct.java
@@ -0,0 +1,16 @@
+import javax.swing.plaf.SplitPaneUI;
+
+public class SecondAct{  
+public static void main(String[] args) {  
+  
+    Student st=new Student();  
+      
+    st.setName("Richel Bacayan"); 
+    
+    st.setEmail("richelbacayan@gmail.com");  
+      
+    System.out.println(st.getName()); 
+    System.out.println(st.getEmail());  
+ 
+}  
+}  
